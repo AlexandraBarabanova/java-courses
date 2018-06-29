48ec3e29fc7b68ccab8c6cea7a3077e343a7ec62
AlexandraBarabanova <sashkamasharova@gmail.com>
2018-06-29 19:11:38 +0300

    create Calculate

---
+++ MyFirstApp.java
@@ -0,0 +1,10 @@
+public class MyFirstApp{
+		public static void main (String[] args) {
+			System.out.println("Hello");
+			int x = 3;
+			String name = "ABC";
+			x = x * 17;
+			System.out.println(name + x + "dgfdhfg");
+			double d = Math.random();
+		}
+}
